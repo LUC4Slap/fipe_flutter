31fade8b309225e5317bda1642c886e113ca03aa
lucas__almeida <lucaslap27@gmail.com>
2023-07-21 13:46:27 -0400

    adiciona config de http

--- lib/pages/fipe.dart
+++ lib/pages/fipe.dart
@@ -1,6 +1,8 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 
 import '../service/api.dart';
+
+const List<String> list = <String>['One', 'Two', 'Three', 'Four'];
 
 class Fipe extends StatefulWidget {
   const Fipe({super.key});
@@ -10,16 +12,52 @@ class Fipe extends StatefulWidget {
 }
 
 class _FipeState extends State<Fipe> {
-  _listar(String pesquisa) {
+  final TextEditingController colorController = TextEditingController();
+  final TextEditingController iconController = TextEditingController();
+  final List<DropdownMenuEntry> colorEntries = <DropdownMenuEntry>[];
+  final List<DropdownMenuEntry> iconEntries = <DropdownMenuEntry>[];
+  String dropdownValue = list.first;
+
+  _listar(String pesquisa) async {
+    print("NA CHAMADA DA API");
     Api api = Api();
-    return api.pesquisar(pesquisa);
+    var resposta = await api.pesquisar(pesquisa);
+    // print(resposta == null ? "SEM RESPOSTA" : resposta);
+    print(resposta);
   }
 
   @override
   Widget build(BuildContext context) {
     _listar("ConsultarTabelaDeReferencia");
-    return const Center(
-      child: Text("AQUI VAI SER A PESQUISA"),
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 20),
+      child: Column(
+        children: [
+          DropdownButton<String>(
+            value: dropdownValue,
+            icon: const Icon(Icons.arrow_downward),
+            elevation: 16,
+            style: const TextStyle(color: Colors.deepPurple),
+            isExpanded: true,
+            underline: Container(
+              height: 2,
+              color: Colors.deepPurpleAccent,
+            ),
+            onChanged: (String? value) {
+              // This is called when the user selects an item.
+              setState(() {
+                dropdownValue = value!;
+              });
+            },
+            items: list.map<DropdownMenuItem<String>>((String value) {
+              return DropdownMenuItem<String>(
+                value: value,
+                child: Text(value),
+              );
+            }).toList(),
+          )
+        ],
+      ),
     );
   }
 }
--- lib/service/api.dart
+++ lib/service/api.dart
@@ -1,13 +1,25 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'dart:async';
 
 class Api {
+  Map<String, String> get headers => {
+        "host": "veiculos.fipe.org.br",
+        "Reference": "http://veiculos.fipe.org.br",
+        "Content-type": "application/json",
+        "User-Agent": "Thunder Client (https://www.thunderclient.com)"
+      };
   Future pesquisar(String pesquisa) async {
-    // &channelId=$ID_CANAL => para pegar de um canal
-    var url =
-        await Uri.parse("http://veiculos.fipe.org.br/api/veiculos/" + pesquisa);
-    var response = await http.post(url);
-    print(response);
+    try {
+      var url = Uri.parse("http://veiculos.fipe.org.br/api/veiculos/$pesquisa");
+      print(url);
+      return await http.post(url, headers: headers);
+    } catch (e) {
+      print("AQUI NO ERRO: $e");
+      return e;
+    }
+    // var url = Uri.parse("http://veiculos.fipe.org.br/api/veiculos/" + pesquisa);
+    // return await http.post(url);
     // if (response.statusCode == 200) {
     //   // print(response);
     //   // Map<String, dynamic> dados = jsonDecode(response.body);
